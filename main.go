db5023a0a031867038d5be43e5e55980a744a313
Sachin Maharana <sachin.nicky@gmail.com>
2019-08-13 21:50:29 +0530

    changes

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is running now")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
